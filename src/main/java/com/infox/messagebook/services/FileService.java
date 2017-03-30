a077dd856e4236bb0307fde001afc17a47492b36
chenrongrong <rongrong.chen@alcatel-sbell.com.cn>
2017-03-30 21:00:29 +0800

    update

---
+++ src/main/java/com/infox/messagebook/services/FileService.java
@@ -0,0 +1,16 @@
+package com.infox.messagebook.services;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+/**
+ * Author: Ronnie.Chen
+ * Date: 2017/3/30
+ * Time: 14:58
+ * rongrong.chen@alcatel-sbell.com.cn
+ */
+@Service
+public class FileService {
+    private Logger logger = LoggerFactory.getLogger(FileService.class);
+}
