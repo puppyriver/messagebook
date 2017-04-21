bbf86fb8b545b60dc5d248e079fa2cc6bcca281e
puppyriver <4278246@qq.com>
2017-04-21 21:44:35 +0800

    1

--- buildAndRun.sh
+++ buildAndRun.sh
@@ -1,3 +1,4 @@
+git pull
 npm run build
 ./gradlew bootRepackage
 java -Djava.security.egd=file:///dev/urandom -Dloader.path=deplibs/ -Dlogging.level=info  -Dspring.profiles.active=onsite  -jar  build/libs/messagebook-1.0-SNAPSHOT.jar &
